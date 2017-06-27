0f280755565db951549830af7abead2ddb1e2fc4
Elie Morisse <syniurge@gmail.com>
2017-06-27 20:54:07 -0300

    object.destroy() should treat class values like structs.
    (cherry picked from commit a81dac04c44b639aee9569762c26564e65de708b)

--- src/object.d
+++ src/object.d
@@ -2540,7 +2540,7 @@ unittest
 }
 
 private void _destructRecurse(S)(ref S s)
-    if (is(S == struct))
+    if (__traits(isAggregateValue, S)) // CALYPSO
 {
     static if (__traits(hasMember, S, "__xdtor") &&
                // Bugzilla 14746: Check that it's the exact member of S.
@@ -2847,7 +2847,7 @@ unittest
     does is done and so that it no longer references any other objects. It does
     $(I not) initiate a GC cycle or free any GC memory.
   +/
-void destroy(T)(T obj) if (is(T == class))
+void destroy(T)(T obj) if (is(T == class) && !__traits(isAggregateValue, T)) // CALYPSO
 {
     rt_finalize(cast(void*)obj);
 }
@@ -2914,7 +2914,7 @@ version(unittest) unittest
 }
 
 /// ditto
-void destroy(T)(ref T obj) if (is(T == struct))
+void destroy(T)(ref T obj) if (__traits(isAggregateValue, T)) // CALYPSO
 {
     _destructRecurse(obj);
     () @trusted {
